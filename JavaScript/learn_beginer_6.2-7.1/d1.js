752bf9646990d3ed52bc142914d25b5d0bffdc26
Alanyaeer <106466417+Alanyaeer@users.noreply.github.com>
2023-06-03 11:19:51 +0800

    first commit

---
+++ JavaScript/learn_beginer_6.2-7.1/d1.js
@@ -0,0 +1,12 @@
+myName = "Chris";
+
+function logName() {
+  console.log(myName);
+}
+const shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
+shopping[0] = "tahini";
+console.log(shopping);
+logName();
+
+myName = 1;
+logName();
